8b893c18109d14c2bb01aee147739349b1ba4177
alanbareiro <alanalfredobareiro@gmail.com>
2021-09-20 16:45:23 -0300

    Se crea estructura de carpetas, las cuales son las siguientes: carpeta .gitignore (dentro de ella esta node_modules), carpeta public (dentro de ella esta css e images), carpeta views (dentro de ella esta archivo index.html el cual esta vacio). Tambien se crea archivo app.js (levanta el servidor local)

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require ("express");
+const path = require("path");
+const app = express();
+
+//listen en localHost 3000
+
+app.listen( 3000 , () => { console.log("Servidor levantado "," http://localhost:3000/index")
+});
+
+
+//.use sirven de archivos estaticos como css e imagenes
+
+const staticFiles = express.static("public");
+app.use(staticFiles);
+
+//get 
+
+// genero una respuesta atravez de un get traigo mi "/views/index.html" con el  metodo path que esta guardado en una variable senFile
+
+app.get("/index", (req,res) => {
+    const fileToSent = path.join(__dirname, "/views/index.html" );
+    res.sendFile(fileToSent);
+});
+
+
+
