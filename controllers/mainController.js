68724c079ad9d1cd1371cf6c469cc5e80c483348
margallego2020 <martingallego_ar@yahoo.com.ar>
2021-11-05 18:57:35 -0300

    Agregado de fs, path y configuración  del archivo en JSON para poder tomarlo en mainController y productsController

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,3 +1,18 @@
+//Permite usar archivos
+const fs = require('fs');
+
+//Permite usar direcciones de rutas
+const path = require('path'); 
+
+
+//Defino en productsFilePath la ruta en donde está el archivo JSON products
+const productsFilePath = path.join(__dirname, '../data/products.json');
+
+/*En products almaceno el contenido del archivo JSON convertido en un array de
+objetos literales*/
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 // Cargo las funciones que quiero que haga
 // Las derivo por render al archivo ejs que le diga
 
@@ -6,7 +21,8 @@ const controller = {
 // Sirve para mostrar la página principal con el listado de productos
 
   index: (req, res) => {
-      res.render('index')
+      res.render('index', { 
+        productsSent : products })
   }
 
 }
--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,3 +1,18 @@
+//Permite usar archivos
+const fs = require('fs');
+
+//Permite usar direcciones de rutas
+const path = require('path'); 
+
+
+//Defino en productsFilePath la ruta en donde está el archivo JSON products
+const productsFilePath = path.join(__dirname, '../data/products.json');
+
+/*En products almaceno el contenido del archivo JSON convertido en un array de
+objetos literales*/
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
 // Cargo las funciones que quiero que haga
 // Las derivo por render al archivo ejs que le diga
 
