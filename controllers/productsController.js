06f8671040a659cee74ce58a6242f902d68e861b
margallego2020 <martingallego_ar@yahoo.com.ar>
2021-11-07 19:37:47 -0300

    Ajustes en formularios, archivos y base de datos para la implementación del CRUD

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -20,7 +20,6 @@ const controller = {
 
     // Derivo cada función a la página que le corresponde
     
-
      //Muestro la lista de productos
 
      index: (req,res) =>{
@@ -50,28 +49,138 @@ const controller = {
         res.render('./products/productCreate')
     },
 
-    // Mensaje producto creado y testeando que lleguen bien los datos por parámetros 
+    // Almaceno los datos cargados en el formulario de creación en la base de datos 
 
     created: (req, res) => {
-        let datosCreados=req.body;
-        console.log(datosCreados);
-        res.send( 'Producto creado')
-              
+
+        let newProduct = {/*Creo el objeto literal almacenando los datos recogidos del formulario
+        por medio del req.body
+        De esta forma hago que coincida con el objeto del formato JSON del archivo de la base de
+        datos */
+
+        id: products[products.length - 1].id + 1, //Es la última posición del id de products + 1
+        productName: req.body.productName,
+        productMainDescription: req.body.productMainDescription,
+        productSecondaryDescription: req.body.productSecondaryDescription,
+        productCategory: req.body.productCategory,
+        productStatus: req.body.productStatus,
+        productPrice: req.body.productPrice,
+        productDiscount: req.body.productDiscount,
+       //Si vino un archivo de imagen lo tomo, sino pongo imagen por default
+        productImage: req.file ? req.file.filename : "default-image.png" 
+        }
+        
+
+        //Agrego los datos del nuevo producto a la base de datos
+
+        //Agrego este nuevo producto a products con un push
+        products.push(newProduct);
+
+        //Guardo products actualizado en la base de datos
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+
+         // Lo redirige a la página de productos donde muestra la lista actualizada
+        res.redirect('/products');
+      
+        
     },
    
-    // Edito los datos de un producto
+    // Busco los datos de un producto y mando el formulario para editarlo (vino por GET)
     
     edit: (req, res) => {
-        res.render('./products/productUpdate')
+    
+        // En la variable id tengo el id del producto a modificar que me mandaron por parámetro en req.params.id
+        const id = req.params.id;
+        
+        /* Busco el producto con la variable id y guardo los datos del producto a modificar en la variable 
+        product*/
+       
+        
+        const product = products.find(product => {
+            return product.id == id;
+        }) 
+
+        // Mando al formulario para hacer la actualización de datos 
+
+        res.render('./products/productUpdate', { productSent : product });
+
     },
 
-     // Mensaje producto modificado y testeando que lleguen bien los datos por parámetros 
 
-     modified: (req, res) => {
-        let datosModificados=req.body;
-        console.log(datosModificados);
-        res.send( 'Producto modificado')
-              
+     // Con los datos actualizados del producto los reemplazo en el archivo (vino por PUT)
+
+     edited: (req, res) => {
+     
+        // En la variable id tengo el id del producto a modificar que me mandaron por parámetro en req.params.id
+        let id = req.params.id;
+        
+        /* Busco el producto con la variable id y guardo los datos del producto a modificar en la variable 
+        product */
+        
+        let productToEdit = products.find(product => {
+            return product.id == id;
+        })
+
+        // Voy a tomar los datos modificados del req.body y los pongo en el producto modificado 
+        let editedProduct =  /* Tiene que ser un objeto literal porque el JSON es un array de objetos
+            literales. */
+            {
+                id: id, //Tomo el id que me pasaron por parámetro
+                productName: req.body.productName,
+                productMainDescription: req.body.productMainDescription,
+                productSecondaryDescription: req.body.productSecondaryDescription,
+                productCategory: req.body.productCategory,
+                productStatus: req.body.productStatus,
+                productPrice: req.body.productPrice,
+                productDiscount: req.body.productDiscount,
+                
+                /*Pregunto si recibo un archivo con la imagen
+                Si me llegó un archivo lo tomo, sino pongo el archivo que vino originalmente */
+                
+                productImage: req.file ? req.file.filename : productToEdit.productImage   
+            }
+
+        // Reemplazo en la posición real del array el producto modificado
+        products.forEach((producto, index) => {
+            if (producto.id == id){
+                products[index] = editedProduct;
+                }
+        });
+
+        //Escribo el archivo de products con la información actualizada
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+
+        //Redirijo a la página del listado de productos
+        
+        res.redirect('/products');
+
+    },
+
+    // Elimino un producto de la base de datos (vino por DELETE)
+    
+    destroy: ( req, res ) => {  
+
+        /* En la variable id tengo el id del producto a eliminar que me mandaron por parámetro en 
+        req.params.id */
+        
+        const id = req.params.id;   
+        
+        /* Para eliminarlo guardo en finalProducts los productos distintos del id que recibí 
+        por parámetro */
+
+        let finalProducts = products.filter(product => {
+            return product.id != id;
+        })
+
+        //Escribo el archivo de products con la información actualizada
+
+        fs.writeFileSync(productsFilePath, JSON.stringify(finalProducts, null, " "));
+
+        //Redirijo a la página del listado de productos
+        
+        res.redirect('/products');
+
     }
     
   }
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -4,30 +4,62 @@ const express = require('express');
 //Habilito la función router para mandar las funciones
 const router = express.Router();
 
+//Defino la variable de multer para poder subir archivos en las peticiones de tipo PUT y POST
+const multer = require('multer');
+
 
 // Guardo en la variable productsController la ruta y el archivo que tiene todas las funciones que está en controllers/productsController.js
 
 const productsController = require('../controllers/productsController');
 
+//Configuro la variable multer
+
+const storage = multer.diskStorage( {
+    destination: function (req, file, cb) {
+        cb(null, './public/images/productos'); //Defino la ubicación en donde se guardan los archivos
+        //Va desde la raiz del proyecto
+    },
+
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname); //Defino cómo quiero nombrar al archivo que quiero guardar
+    }
+});
+
+const upload = multer ({storage}); //Ejecuto multer pasando como objeto literal la constante storage
+
+
 
 // Con router, de acuerdo a la ruta que ponga, le digo que haga tal función 
+
+//Ver el carro de compras
+router.get('/cart', productsController.cart);
 
 //get de product
 router.get('/',productsController.index);
 
+//Creación de un producto
+router.get('/create/', productsController.create);
+
 //get de product con id
-router.get('/:id', productsController.productDetail);
+router.get('/list/:id', productsController.productDetail);
 
-router.get('/cart', productsController.cart);
 
-router.get('/create', productsController.create);
+//Almaceno la información cargada del formulario en la base de datos
+router.post('/', upload.single('newProductImage'), productsController.created); 
 
-router.post('/create/', productsController.created);
 
-router.get('/edit/', productsController.edit);
+//Edición del producto
 
-router.put('/edit/', productsController.modified);
+/*Llamo los datos con el id del producto a editar y cargo el formulario con los datos anteriores 
+para editar los datos */
+router.get('/edit/:id/', productsController.edit);
 
+//Modifico los datos del producto y actualizo la base de datos
+router.put('/edit/:id/', upload.single('editedProductImage'), productsController.edited); /*Hay que modificar 
+//con esto en el form del productUpdate*/ 
+
+//Eliminación del producto
+router.delete('/delete/:id', productsController.destroy);
 
 
 module.exports = router;
