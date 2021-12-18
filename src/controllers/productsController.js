03ecef2a8831a9452d0f69b1a6dfd6c9af9b634b
alanbareiro <alanalfredobareiro@gmail.com>
2021-12-18 19:19:43 -0300

    Implementacion sequelize en products, queda pendiente para finalizar solucionar que me lea los campos status y category de la base de datos cuando edito un producto, tambien que me mantenga la imagen en caso de que no la modifique tambien n edit

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -138,28 +138,25 @@ const controller = {
     
     edit: (req, res) => {
        
-/*
+
         
-        let promProducts = Product.findByPk(productId,{include: ['category','status']});
-        let promCategory = Category.findAll();
-        let promStatus = Status.findAll();*/
         let productId = req.params.id;
-        Product.findByPk(productId)
+        let promProducts = Product.findByPk(productId);
+        let promCategory = Category.findAll();
+        let promStatus = Status.findAll();
+        
+        /*Product.findByPk(productId)
 
             .then(product=>{
                 res.render ('./products/productUpdate', {product})
-        });
+        });*/
 
-        
-
-        /*
         Promise
-        .all([promProducts, promCategory, promStatus])
-        .then(([product, allCategories, allStatuses]) => {
-            return res.render('./products/productUpdate', {product, allCategories, allStatuses})})
+        .all([promProducts,promCategory, promStatus])
+        .then(([product,allCategories, allStatuses]) => {
+            return res.render('./products/productUpdate', {product,allCategories,allStatuses})})
         .catch(error => res.send(error))
-        */
-    
+        
         //Logica de JSON
 
         /* En la variable id tengo el id del producto a modificar que me mandaron por parámetro en req.params.id*/
@@ -183,21 +180,49 @@ const controller = {
      // Con los datos actualizados del producto los reemplazo en el archivo (vino por PUT)
 
      edited: (req, res) => {
-     
+
+         /*falta la funcionalidad que si no se agrega una imagen nueva te deje por defecto la imagen anterior
+         ahora esta funcioando que si no se coloca una imagen nueva te pisa la vieja por una por defecto*/
+
+        Product.update(
+            {
+                name: req.body.name,
+                mainDescription: req.body.mainDescription,
+                secondaryDescription: req.body.secondaryDescription,
+                categoryId: req.body.category,
+                statusId: req.body.status,
+                price: req.body.price,
+                discount: req.body.discount,
+                //Si vino un archivo de imagen lo tomo, sino pongo imagen por default
+                image: req.file ? req.file.filename : "default-image.png" 
+            },
+            {
+                where: {id:req.params.id}
+            }
+        )
+            .then(()=>{
+                return res.redirect('/products')
+            })
+
+
+        },
+
+
+     /*
         // En la variable id tengo el id del producto a modificar que me mandaron por parámetro en req.params.id
         let id = req.params.id;
         
         /* Busco el producto con la variable id y guardo los datos del producto a modificar en la variable 
         product */
         
-        let productToEdit = products.find(product => {
+       /* let productToEdit = products.find(product => {
             return product.id == id;
-        })
+        })*/
 
         // Voy a tomar los datos modificados del req.body y los pongo en el producto modificado 
-        let editedProduct =  /* Tiene que ser un objeto literal porque el JSON es un array de objetos
-            literales. */
-            {
+       // let editedProduct =  /* Tiene que ser un objeto literal porque el JSON es un array de objetos
+        //    literales. 
+         /*   {
                 id: id, //Tomo el id que me pasaron por parámetro
                 name: req.body.name,
                 mainDescription: req.body.mainDescription,
@@ -206,43 +231,54 @@ const controller = {
                 status: req.body.status,
                 price: req.body.price,
                 discount: req.body.discount,
-                
+              */  
                 /*Pregunto si recibo un archivo con la imagen
                 Si me llegó un archivo lo tomo, sino pongo el archivo que vino originalmente */
                 
-                image: req.file ? req.file.filename : productToEdit.image   
-            }
+              //  image: req.file ? req.file.filename : productToEdit.image   
+            //}
 
         // Reemplazo en la posición real del array el producto modificado
-        products.forEach((producto, index) => {
+        /*products.forEach((producto, index) => {
             if (producto.id == id){
                 products[index] = editedProduct;
                 }
         });
-
+        */
         //Escribo el archivo de products con la información actualizada
 
-        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
+        //fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "));
 
         //Redirijo a la página del listado de productos
         
-        res.redirect('/products');
+        //res.redirect('/products');
 
-    },
+   
 
     // Elimino un producto de la base de datos (vino por DELETE)
     
     destroy: ( req, res ) => {  
 
+        Product.destroy(
+            {
+                where:{id:req.params.id}
+            }
+        )
+        .then(()=>{
+            return res.redirect('/products')
+        })
+
+
+
         /* En la variable id tengo el id del producto a eliminar que me mandaron por parámetro en 
         req.params.id */
         
-        const id = req.params.id;   
+        //const id = req.params.id;   
         
         /* Para eliminarlo guardo en finalProducts los productos distintos del id que recibí 
         por parámetro */
 
-        let finalProducts = products.filter(product => {
+        /*let finalProducts = products.filter(product => {
             return product.id != id;
         })
 
@@ -252,10 +288,20 @@ const controller = {
 
         //Redirijo a la página del listado de productos
         
-        res.redirect('/products');
+        res.redirect('/products');*/
 
     }
     
   }
   
   module.exports = controller;
+
+  /*<% allStatuses.forEach(status => { %>
+                        <option value="<%= status.id %>" <% if (product.statusId == status.id) { %><%= 'selected' %><% }; %>><%= status.name %></option>
+                        <% }); %>
+                        
+                        
+    <% allCategories.forEach(category => { %>
+                      <option value="<%= category.id %>" <% if (product.categoryId == category.id) { %><%= 'selected' %><% }; %>><%= category.name %></option>
+                      <% }); %>
+                      */
